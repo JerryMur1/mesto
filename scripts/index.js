4d3615f71721d10f703fd998e0e9dbd7cc7dba0f
JerryMur1 <eeests1@yandex.ru>
2020-10-03 19:58:41 +0300

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
 let editButton = document.querySelector(".button_type_edit");
 let closeButton = document.querySelector(".button_type_close");
 let popUp = document.querySelector(".popup");
-let nameInput = document.querySelector(".popup__name_first");
-let jobInput = document.querySelector(".popup__name_second");
+let nameInput = document.querySelector(".popup__name_type_title");
+let jobInput = document.querySelector(".popup__name_type_subtitle");
 let titleName = document.querySelector(".profile__title");
 let titleJob = document.querySelector(".profile__subtitle");
 
 let popUpToggle = () => {
     popUp.classList.toggle("popup_is-opened");
+};
+let popUpCopy = () => {
     nameInput.value = titleName.textContent;
     jobInput.value = titleJob.textContent;
-}
+    popUpToggle();
+};
 
-editButton.addEventListener("click", popUpToggle);
+editButton.addEventListener("click", popUpCopy);
 closeButton.addEventListener("click", popUpToggle);
 
 
